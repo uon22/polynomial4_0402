0a6f859a4c4077e8ac2c1395018461d7261919c2
uon22 <kmk0013@naver.com>
2024-04-02 10:55:15 +0900

    초기 커밋.

---
+++ polynomial4_0402.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+#define MAX_TERMS 101 //상수 사용할때 쓰면 좋음, 8줄 struct term terms[MAX_TERMS];와 연계
+typedef struct {
+	float coef;
+	int expon;
+} plynomial;
+polynomial terms[MAX_TERMS] = { {8,3}, {7,1}, {1,0}, {10,3}, {3,2}, {1,0} };
+int avail = 6;
+
+char compare(int a, int b)
+{
+	if (a > b) return '>';
+	else if (a == b) return '=';
+	else return '<';
+}
+
+void attach(float coef, int expon)
+{
+	if (avail > MAX_TERMS) {
+		fprintf(stderr, "항의 개수가 너무 많음|n");
+		exit(1);
+	}
+	terms[avail].coef = coef;
+	terms[avail].expon = expon;
+	avail++;
+}
+
+
+void poly_add2(int As, int Ae, int Bs, int Be, int* Cs, int* Ce)
+{
+	float tempcoef;
+	*Cs = avail;
+	while (As <= Ae && Bs <= Be)
+		switch (compare(terms[As].expon, terms[Bs]. expon)) {}
+}
